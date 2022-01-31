7cd6df43fa2ba1a1cd4570a6d461a2a7e05ba18a
rryaboshtan <rrvworkroman@gmail.com>
2022-01-31 11:34:32 +0200

    without host

--- service/mail-service.js
+++ service/mail-service.js
@@ -2,20 +2,17 @@ const nodemailer = require('nodemailer');
 
 class MailService {
    constructor() {
-      this.transporter = nodemailer.createTransport(
-         {
-            host: process.env.SMTP_HOST,
-            port: process.env.SMTP_PORT,
-            secure: false,
-            auth: {
-               user: process.env.SMTP_USER,
-               pass: process.env.SMTP_PASSWORD,
-            },
+      this.transporter = nodemailer.createTransport({
+         // host: process.env.SMTP_HOST,
+         // port: process.env.SMTP_PORT,
+         // secure: false,
+         service: 'gmail',
+
+         auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASSWORD,
          },
-         {
-            from: 'Mailer Test rrvwork27@gmail.com',
-         }
-      );
+      });
    }
    async sendActivationMail(to, link) {
       await this.transporter.sendMail({
